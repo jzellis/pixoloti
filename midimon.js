387ecbbcce42a58fbdfef99eafe0e823135b4e84
Joshua Ellis <jzellis@gmail.com>
2017-03-28 23:24:12 -0700

    Got LCD working but still has odd errors and segfaults

--- midimon.js
+++ midimon.js
@@ -1,4 +1,12 @@
 var midi = require('midi'),
+Lcd = require('lcd'),
+lcd = new Lcd({
+rs: 25,
+e: 24,
+data: [23, 17, 21, 22],
+cols: 16,
+rows: 2
+}),
 codes = {
   9: "",
   8: "",
@@ -10,7 +18,7 @@ notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 var input = new midi.input();
 
 var ip = require("ip");
-console.log ( ip.address() );
+
 
 
 function dec2bin(dec){
@@ -24,11 +32,18 @@ let ports = [];
 
 console.log("Ch Note/CC Val");
 
+	lcd.on('ready', function() {
+			lcd.clear();
+		  lcd.setCursor(0,0);
+	lcd.print(ip.address());
+	  });
 
 for(var i = 0; i < portCount; i++){
+	console.log(input.getPortName(i));
   ports[i] = new midi.input();
+  
   ports[i].on('message', function(deltaTime, msg) {
-
+console.log(msg);
 event = parseInt((dec2bin(msg[0]) + "0000").substring(0,4),2);
 channel = parseInt("0000" + dec2bin(msg[0]).substring(4),2);
 if(event != 8){
@@ -42,12 +57,21 @@ if(event != 8){
       value = msg[1];
     }
     formatted = channel + " " + codes[event] + value + " " + msg[2]
+
+
+  }
+  			//~ lcd.clear();
+		  //~ lcd.setCursor(0,0);
+	//~ lcd.print(ip.address());
+	lcd.setCursor(0,1);
+	lcd.print(formatted, function(err){
+		if(err) throw err});
 console.log(formatted);
-}
-  });
+});
   ports[i].openPort(i);
 
 }
+
 
 process.on('SIGINT', function(){
 // Close the port when done.
@@ -55,4 +79,7 @@ process.on('SIGINT', function(){
 for(var i = 0; i < portCount; i++){
 ports[i].closePort()
 }
+lcd.clear();
+lcd.close();
+process.exit();
 });
